4645005e06123a00097058562fe1a0750c331df6
Al-Faruk <alfaruksow@gmail.com>
2023-02-20 08:12:15 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point toujours commencer par ca a part le include
+ *
+ * Return: Always 0 (Success) ne pas oublierles points virgules
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
